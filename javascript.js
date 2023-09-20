8a18824379bfd807c3e6f7daa43647aff8864248
Tanushri Jain <jaintanushri34@gmail.com>
2023-09-20 00:19:36 -0500

    contact and other styling changes

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
-const btn = document.createElement('button');
-btn.innerHTML = 'hello button';
-document.body.append(btn);
-btn.style.backgroundColor = 'pink';
+// const btn = document.createElement('button');
+// btn.innerHTML = 'hello button';
+// document.body.append(btn);
+// btn.style.backgroundColor = 'pink';
+
+function submit() {
+  const form = document.getElementById(form_id).ariaValueMax;
+  if (form !== '0') {
+    return alert('Meassage sent successfully');
+  }
+}
